b66875fb0b371630b140dbde3dd3ff72fed7a99a
Jason McElwaine <jasonmcelwaine@Jasons-Mac-mini.local>
2021-06-29 19:18:36 -0700

    Update with-context.js

--- admin/src/js/context/with-context.js
+++ admin/src/js/context/with-context.js
@@ -9,34 +9,34 @@ import React from 'react';
 */
 
 import { 
-	AppContext,
+  AppContext,
   AdminContext,
-	EditorContext,
+  EditorContext,
   OverviewContext,
 } from '../context';
 
 const withAppContext = Component => ( props => (
-	<AppContext.Consumer>
-		{ context => <Component appContext={ context } { ...props } /> }
-	</AppContext.Consumer>
+  <AppContext.Consumer>
+    { context => <Component appContext={ context } { ...props } /> }
+  </AppContext.Consumer>
 ) );
 
 const withAdminContext = Component => ( props => (
-	<AdminContext.Consumer>
-		{ context => <Component adminContext={ context } { ...props } /> }
-	</AdminContext.Consumer>
+  <AdminContext.Consumer>
+    { context => <Component adminContext={ context } { ...props } /> }
+  </AdminContext.Consumer>
 ) );
 
 const withOverviewContext = Component => ( props => (
-	<OverviewContext.Consumer>
-		{ context => <Component overviewContext={ context } { ...props } /> }
-	</OverviewContext.Consumer>
+  <OverviewContext.Consumer>
+    { context => <Component overviewContext={ context } { ...props } /> }
+  </OverviewContext.Consumer>
 ) );
 
 const withEditorContext = Component => ( props => (
-	<EditorContext.Consumer>
-		{ context => <Component editorContext={ context } { ...props } /> }
-	</EditorContext.Consumer>
+  <EditorContext.Consumer>
+    { context => <Component editorContext={ context } { ...props } /> }
+  </EditorContext.Consumer>
 ) );
 
 export {
