e217f755c3e7eda7c9f90d3f24914075553cccf3
Jason McElwaine <jasonmcelwaine@Jasons-Mac-mini.local>
2021-07-21 18:41:15 -0700

    add skin/loading submenus

--- admin/src/js/editor/grid-settings/grid-settings-options/options.js
+++ admin/src/js/editor/grid-settings/grid-settings-options/options.js
@@ -3,10 +3,14 @@
  */
 import Naming from './settings/naming';
 import Source from './settings/source';
+import Skin from './settings/skin';
+import Loading from './settings/loading';
 
 const options = {
   naming: Naming,
   source: Source,
+  skin: Skin,
+  loading: Loading,
 };
 
 export default options;
